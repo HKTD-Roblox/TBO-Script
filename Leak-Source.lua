59f616b5ebcefb1059adfa31fd3c89fd4f7274f1
Hoàng Kim Tiến Đạt <thuykieu071980@gmail.com>
2026-09-16 11:09:49 +0700

    Update Leak-Source.lua

--- Leak-Source.lua
+++ Leak-Source.lua
@@ -1,4 +1,4 @@
-local hwid = "IDK"
+local hwid = "unknown"
 pcall(function() if gethwid then hwid = gethwid() else hwid = game:GetService("RbxAnalyticsService"):GetClientId() end end)
 
 local url = "https://peeky.pythonanywhere.com/DaHoodGui" .. game:GetService("HttpService"):UrlEncode(hwid) .. "&_cb=" .. tostring(os.clock())
